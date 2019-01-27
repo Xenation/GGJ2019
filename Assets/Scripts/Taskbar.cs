05b1ec6e9e908c5c2d6a4711def70715fd299621
floalazais <flo.alazais@gmail.com>
2019-01-27 15:25:49 +0100

    ajout blue bar

--- Assets/Scripts/Taskbar.cs
+++ Assets/Scripts/Taskbar.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Taskbar : MonoBehaviour
 {
+    public GameObject blueBarEdge;
+    public GameObject blueBarFolder;
     public Texture2D normalTexture;
     public Texture2D errorTexture;
     Sprite normalSprite;
@@ -19,12 +21,44 @@ public class Taskbar : MonoBehaviour
         image = GetComponent<Image>();
 
         set_normal_texture_to_taskbar();
+
+        blueBarEdge.SetActive(false);
+        blueBarFolder.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        ;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            set_blue_bar_under_edge();
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            set_blue_bar_under_folder();
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            hide_blue_bar();
+        }
+    }
+
+    public void set_blue_bar_under_edge()
+    {
+        blueBarEdge.SetActive(true);
+        blueBarFolder.SetActive(false);
+    }
+
+    public void set_blue_bar_under_folder()
+    {
+        blueBarEdge.SetActive(false);
+        blueBarFolder.SetActive(true);
+    }
+
+    public void hide_blue_bar()
+    {
+        blueBarEdge.SetActive(false);
+        blueBarFolder.SetActive(false);
     }
 
     public void set_normal_texture_to_taskbar()
